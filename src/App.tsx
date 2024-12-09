085f59fbe214904df7ee9b1e4a94912a8b65c282
sisaung <sisaungvipse780@gmail.com>
2024-12-10 00:58:24 +0630

    init: make config

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "./components/LoginForm";
+
+const App = () => {
+  return (
+    <div className="w-full h-screen mx-auto flex flex-col justify-center items-center">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default App;
